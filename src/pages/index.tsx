47a5507d0eb1bf86f53316f8600b62c6d0e6c55b
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2021-04-21 14:02:48 -0300

    Using fetch SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,23 @@
-//SPA
-export default function Home() {
-  fetch('http://localhost:3333/episodes')
-  .then(episodes => episodes.json())
-  .then(data => console.log(data));
+//SSR
+export default function Home(props) {
+
   return (
+    <div>
       <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+      
   )
+}
+
+export async function getServerSideProps(){
+    const response =  await  fetch('http://localhost:3333/episodes');
+    const episodes = await response.json();
+
+    return {
+      props: {
+        episodes:   episodes,
+      }
+    }
+    
 }
