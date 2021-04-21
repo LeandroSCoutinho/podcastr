33b0a34efc17a81d351a69496d8500bb173e2691
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2021-04-21 13:51:53 -0300

    Using fetch SPA

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
-
-
+//SPA
 export default function Home() {
+  fetch('http://localhost:3333/episodes')
+  .then(episodes => episodes.json())
+  .then(data => console.log(data));
   return (
       <h1>Index</h1>
   )
